4f2a61c2176d0aef2f06534657766ed78d0aaf19
robertmichael092 <robertmichael092@gmail.com>
2025-12-03 01:26:57 +0300

    Update ci_build.sh

--- ci_build.sh
+++ ci_build.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+set -e
+
+# Skip the "Buildozer is running as root" prompt
+export BUILDOZER_WARN_ON_ROOT=0
+
+# Force yes to all prompts
+export BUILDOZER_ACCEPT=1
+export BUILDOZER_ALLOW_ROOT=1
+
+# Navigate to the app folder (GitHub mounts repo at /app)
+cd /app
+
+# Run buildozer
+buildozer -v android debug
